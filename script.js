62ea721ca08434ee6c2ebf4f9a42243752ab8c45
Jawadmarmar <marmarjawad6@gmail.com>
2025-09-26 09:37:27 +0300

    finishing

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let name="john";
+
+let age=20;
+
+console.log(`two years ago: 
+Hi my name is ${name} and I'm ${age} years old`);
+
+age=22;
+
+console.log(`Now: 
+Hi my name is ${name} and I'm ${age} years old`);
